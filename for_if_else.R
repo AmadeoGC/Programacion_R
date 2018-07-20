9b1a6b825697d91394c4e51211d1d24deac50626
AmadeoGC <40497655+AmadeoGC@users.noreply.github.com>
2018-07-20 13:29:54 -0400

    Update for_if_else.R

--- for_if_else.R
+++ for_if_else.R
@@ -1,18 +1,25 @@
 ##########################################################
-###           ELEMETOS DE PROGRAMACI”N EN R            ###
+###           ELEMETOS DE PROGRAMACI√ìN EN R            ###
 ###                 bucles -> for()                    ###
-###              condiciÛn -> if/else                  ###
-###               <Amadeo Guzm·n C.>                   ###
+###              condici√≥n -> if/else                  ###
+###               <Amadeo Guzm√°n C.>                   ###
 ##########################################################
 
 
 ### Bucle (for) + condicional (if) con numeros
 #-----------------------------------------------
 
-#primero declarar el vector(objeto) que guardara los resultados
+#el ejemplo m√°s b√°sico de un bucle
+for (i in 1:10) {
+  print(i)
+}
+
+#si queremos guardar el resultado en un vector, lo primero es declarar el vector(objeto) 
+#que guardar√° los resultados
 clasif1 <- NULL
 
-#correr programa -> un programa es una secuencia de cÛdigos que busca resolver o ejecutar una tarea
+#correr programa -> un programa es una secuencia de c√≥digos que busca resolver o ejecutar una tarea
+#tarea: etiquetar como "grande" los numeros mayores a 5
 for (i in 1:10) {
   if (i > 5) {
     clasif1[i] <- 'grande' 
@@ -20,7 +27,6 @@ for (i in 1:10) {
 }
 #leemos nuestro vector
 clasif1
-
 
 
 #ciclo for + if + else
@@ -43,7 +49,7 @@ clasif
 ### Como crear una nueva columna en un data frame -> for() + if() + else
 #-----------------------------------------------------------------------
 
-#creamos un pequeÒo data frame
+#creamos un peque√±o data frame
 r <- c(1,2,3,4,5,6,7,8,3,5,10) 
 p <- c("v1","v2","v3","v4","v5","v6","v7","v8","V9","v10", "v11")
 
@@ -55,19 +61,19 @@ length(xx$r)
 z <- NULL
 for (i in 1:length(xx$r)) {
   if (xx$r[i] > 5) {
-    z[i] <- "n∞ grande"
+    z[i] <- "n¬∞ grande"
   } else {
-    z[i] <- "n∞ chico"
+    z[i] <- "n¬∞ chico"
   }
 }
 #leemos nuestro nuevo vector
 z
 
 
-#================================================================================================================================================================================================
+#=================================================================================================================================================================================
 
 
-### Como crear un nuevo vector con doble condiciÛn [3 categorÌas]
+### Como crear un nuevo vector con doble condici√≥n [3 categor√≠as]
 #-------------------------------------------------------------------
 
 #vector inicial
@@ -93,12 +99,29 @@ nuevo
 #====================================================================================================================================================================
 
 
-# Otra forma de ocupar for() e if()
-#-------------------------------------------------------------
+# Condici√≥n de parada -> break
+#--------------------------------
 
-for(member in c("Laurene","Tim","Sofia","Jonatan","Philipp","Katharina")) {
-  cat(sprintf("Hello %s...\n",member))
+cities <- list("New York", "Paris",
+                 "London", "Tokyo",
+                 "Rio de Janeiro", "Cape Town")
+
+for(city in cities) {
+  if(nchar(city) == 6) {
+    break
+  }
+  print(city)
 }
+
+
+
+for(city in cities) {
+  if(nchar(city) == 6) {
+    next
+  }
+  print(city)
+}
+
 
 
 
